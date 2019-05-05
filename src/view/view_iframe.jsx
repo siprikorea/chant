4b14f8f7559ef4d26ced9953e048d7876def5e15
Chanbong Park <siprikorea@gmail.com>
2019-05-05 23:51:47 +0900

    Fixed

--- src/view/view_iframe.jsx
+++ src/view/view_iframe.jsx
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 class ViewIFrame extends Component {
   render() {
     const { src, show } = this.props;
-    return <iframe id='sheet' title='sheet' src={src} style={{ border: 0, width: '100%', height: '100%', display: show ? 'block' : 'none' }} />
+    return <iframe id='sheet' title='sheet' src={src} style={{ border: 0, width: '100%', height: '1000px', display: show ? 'block' : 'none' }} />
   }
 }
 
